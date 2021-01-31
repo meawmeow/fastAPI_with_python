695cb8b84a18abe89d4a22d408feaa127d233572
kittipong <meowcoding7044@gmail.com>
2021-01-31 14:42:57 +0700

    “bgtask”

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import Depends, FastAPI
 
-from routers import items
+from routers import items,bgtask
 from auths import authJwt
 
-app = FastAPI()
+
+app = FastAPI(
+    title="My Learn FastAPI",
+    description="This is a very fancy project, with auto docs for the API and everything",
+)
 
 app.include_router(items.router)
+app.include_router(bgtask.router, 
+    prefix="/task",
+    tags=[" background task"])
 app.include_router(authJwt.router,
     prefix="/admin",
     tags=["Authentication"])
